696513ec819d1fddc2e8b782d5c490201d9063ec
amaple0229 <amaple0229@gmail.com>
2017-05-26 11:35:32 +0800

    [Update] Update release package rev:294
    1. Update release package rev:294
    
    Reviewer: Maple

---
+++ WorkLib/BIN/Customer.d
@@ -0,0 +1,44 @@
+BIN/Customer.o: SRC/Customer.c INC/ZetDef.h INC/Nds.h INC/Vender.h \
+ INC/Define.h INC/Revision.h INC/SubRevision.h INC/TypeDef.h \
+ INC/ZET7100_Region.h INC/DataFlash.h INC/ZetDF.H INC/ICSetup.h \
+ INC/ZET7100.h INC/VarDef.h INC/ZetDEF.h INC/FuncDef.h \
+ INC/ZET7100_Trace.h INC/ICDeviceName.h INC/DFBitDefine.h \
+ INC/SramBitDefine.h
+
+INC/ZetDef.h:
+
+INC/Nds.h:
+
+INC/Vender.h:
+
+INC/Define.h:
+
+INC/Revision.h:
+
+INC/SubRevision.h:
+
+INC/TypeDef.h:
+
+INC/ZET7100_Region.h:
+
+INC/DataFlash.h:
+
+INC/ZetDF.H:
+
+INC/ICSetup.h:
+
+INC/ZET7100.h:
+
+INC/VarDef.h:
+
+INC/ZetDEF.h:
+
+INC/FuncDef.h:
+
+INC/ZET7100_Trace.h:
+
+INC/ICDeviceName.h:
+
+INC/DFBitDefine.h:
+
+INC/SramBitDefine.h:
---
+++ WorkLib/BIN/Main.d
@@ -0,0 +1,43 @@
+BIN/Main.o: SRC/Main.c INC/ZetDEF.h INC/Nds.h INC/Vender.h INC/Define.h \
+ INC/Revision.h INC/SubRevision.h INC/TypeDef.h INC/ZET7100_Region.h \
+ INC/DataFlash.h INC/ZetDF.H INC/ICSetup.h INC/ZET7100.h INC/VarDef.h \
+ INC/ZetDEF.h INC/FuncDef.h INC/ZET7100_Trace.h INC/ICDeviceName.h \
+ INC/DFBitDefine.h INC/SramBitDefine.h
+
+INC/ZetDEF.h:
+
+INC/Nds.h:
+
+INC/Vender.h:
+
+INC/Define.h:
+
+INC/Revision.h:
+
+INC/SubRevision.h:
+
+INC/TypeDef.h:
+
+INC/ZET7100_Region.h:
+
+INC/DataFlash.h:
+
+INC/ZetDF.H:
+
+INC/ICSetup.h:
+
+INC/ZET7100.h:
+
+INC/VarDef.h:
+
+INC/ZetDEF.h:
+
+INC/FuncDef.h:
+
+INC/ZET7100_Trace.h:
+
+INC/ICDeviceName.h:
+
+INC/DFBitDefine.h:
+
+INC/SramBitDefine.h:
